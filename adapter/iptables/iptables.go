ede01fd056f4090786e891798204c6698d9b7e0b
IdeaMeshDyx <ideameshdyx@gmail.com>
2023-08-01 07:19:42 -0700

    add more logic but some bugs need to fixed

--- adapter/iptables/iptables.go
+++ adapter/iptables/iptables.go
@@ -14,10 +14,10 @@ type Rule struct {
 
 // iptable Operation include all the operation to interact with iptables
 type IptOp interface {
-	// insert amount of rules in the table
+	// insert rules in the table
 	Append(rule Rule) error
 
-	// insert single rule into the table
+	// insert single rule into the position of the table
 	InsertUnique(rule Rule, pos int) error
 
 	// delete rule from the table
@@ -32,90 +32,125 @@ type IptOp interface {
 	// clear the chain
 	ClearChain(table string, chain string) error
 
-	// delete the chain
-	DeleteChain(table string, chain string) error
-
 	// create a new chain
 	NewChain(table string, chain string) error
-
-	// list all the chains
-	ListChains(table string) ([]string, error)
 }
 
-// iptable Adapter to control the rules
-type Adapter struct {
+// iptable Iptutil to control the rules
+type Iptutil struct {
 	// coreos/go-iptables added  contains filtered or unexported fields
-	*iptables.IPTables
+	ipt *iptables.IPTables
 
-	// rules need to append
+	// rules need to append, and it should be a queue to store the respec
 	ruleSet []Rule
 }
 
 // init operator to control ipatbles
 func New() (IptOp, error) {
 	// init ipatbles operator for ipv4 rules
+	// TODO: add IPV6 support
 	ipt, err := iptables.New(iptables.IPFamily(iptables.ProtocolIPv4), iptables.Timeout(5))
 
 	if err != nil {
-		klog.Errorf("Failed to create IpOpt : %v", err)
+		klog.Errorf("Failed to create iptables operator : %v", err)
 		return nil, err
 	}
-	klog.Infof("create IpOpt success")
+	klog.Infof("create iptables operator success")
 
-	return &Adapter{ipt}, nil
+	return &Iptutil{ipt}, nil
 }
 
-func (adapter *Adapter) Append(rule Rule) error {
-	ok, err := adapter.Exists(rule)
-	// if there is no such rule then add them
+func (iptutil *Iptutil) Append(rule Rule) error {
+	// check if the rule is in the table
+	ok, err := iptutil.Exists(rule)
+	// if there is no such rule then add it
 	if err == nil && !ok {
-		err = adapter.Append(rule)
+		err = iptutil.ipt.AppendUnique(rule)
+		klog.Infof("this rule is not exist, start to insert it")
 	}
 	if err != nil {
-		klog.ErrorS(err, "error on iptables.Append", "table", rule.table, "chain", rule.chain, "rulespec", rulespec, "exists", exists)
+		klog.Errorf("error on iptables.AppendUnique in table %v on chain %v for rule %v, err ", rule.table, rule.chain, rule.spec, err)
 		return err
 	}
-	if klog.V(5).Enabled() {
-		klog.V(5).InfoS("iptables.Append succeeded", "table", table, "chain", chain, "rulespec", rulespec, "exists", exists)
-	}
+	klog.Infof("iptables.AppendUnique succeeded in table %v on chain %v for rule %v", rule.table, rule.chain, rule.spec)
+
 	return nil
 }
 
-func (adapter *Adapter) InsertUnique(rule Rule, pos int) error {
-	exists, err := adapter.Exists(table, chain, rulespec...)
+func (iptutil *Iptutil) InsertUnique(rule Rule, pos int) error {
+	exists, err := iptutil.Exists(rule)
+	// if there is no such rule then add it in the special position
 	if err == nil && !exists {
-		err = adapter.Insert(table, chain, pos, rulespec...)
+		err = iptutil.ipt.Insert(rule.table, rule.chain, pos, rule.spec)
+		klog.Infof("this rule is not exist, start to insert it on the position %v", pos)
 	}
 	if err != nil {
-		klog.ErrorS(err, "error on iptables.Insert", "table", table, "chain", chain, "pos", pos, "rulespec", rulespec, "exists", exists)
+		klog.Errorf("error on iptables.AppendUnique in table %v on chain %v  at position %v for rule %v, err ", rule.table, rule.chain, pos, rule.spec, err)
 		return err
 	}
-	if klog.V(5).Enabled() {
-		klog.V(5).InfoS("iptables.Insert succeeded", "table", table, "chain", chain, "pos", pos, "rulespec", rulespec, "exists", exists)
-	}
+	klog.Infof("iptables.AppendUnique succeeded in table %v on chain %v at position %v for rule %v", rule.table, rule.chain, pos, rule.spec)
 	return nil
 }
 
-func (adapter *Adapter) Delete(rule Rule) error {
-	err := adapter.IPTables.DeleteIfExists(table, chain, rulespec...)
+func (iptutil *Iptutil) Delete(rule Rule) error {
+	// delete the rule
+	err := iptutil.ipt.DeleteIfExists(rule.table, rule.chain, rule.spec)
 	if err != nil {
-		klog.ErrorS(err, "error on iptables.Delete", "table", table, "chain", chain, "rulespec", rulespec)
+		klog.Errorf("error on iptables.Delete in table %v on chain %v for rule %v, err ", rule.table, rule.chain, rule.spec, err)
 		return err
 	}
-	if klog.V(5).Enabled() {
-		klog.V(5).InfoS("iptables.Delete succeeded", "table", table, "chain", chain, "rulespec", rulespec)
-	}
+	klog.Infof("iptables.Delete succeeded in table %v on chain %v for rule %v", rule.table, rule.chain, rule.spec)
 	return nil
 }
 
-func (adapter *Adapter) Exists(rule Rule) (bool, error) { return true, nil }
+func (iptutil *Iptutil) Exists(rule Rule) (bool, error) {
+	// check if the rule exist in the table
+	exists, err := iptutil.ipt.Exists(rule.table, rule.chain, rule.spec)
+	if err == nil && !exists {
+		klog.Infof("rule is not exist in table %v on chain %v", rule.table, rule.chain, rule.spec)
+		return false, nil
+	}
+	if err != nil {
+		klog.Errorf("error on iptables.Exists in table %v on chain %v for rule %v, err ", rule.table, rule.chain, rule.spec, err)
+		return false, err
+	}
+	klog.Infof("iptables rules exist in table %v on chain %v", rule.table, rule.chain)
+	return true, nil
+}
 
-func (adapter *Adapter) List(table string, chain string) ([]string, error) { return nil, nil }
+func (iptutil *Iptutil) List(table string, chain string) ([]string, error) {
+	// get the rule from the table/chain
+	rules, err := iptutil.ipt.List(table, chain)
+	if err != nil {
+		klog.Errorf("error on iptables.List in table %v on chain %v  err ", table, chain, err)
+		return nil, err
+	}
+	klog.Infof("iptables.List succeeded, in table %v on chain %v get rules %v", table, chain, rules)
+	return rules, nil
+}
 
-func (adapter *Adapter) ClearChain(table string, chain string) error { return nil }
+func (iptutil *Iptutil) ClearChain(table string, chain string) error {
+	// clear the chain
+	err := iptutil.ipt.ClearAndDeleteChain(table, chain)
+	if err != nil {
+		klog.Errorf("error on iptables.ClearAndDeleteChain in table %v on chain %v  err ", table, chain, err)
+		return nil, err
+	}
+	klog.Infof("iptables.ClearAndDeleteChain succeeded, in table %v on chain %v ", table, chain)
+	return nil
+}
 
-func (adapter *Adapter) DeleteChain(table string, chain string) error { return nil }
-
-func (adapter *Adapter) NewChain(table string, chain string) error { return nil }
-
-func (adapter *Adapter) ListChains(table string) ([]string, error) { return nil, nil }
+func (iptutil *Iptutil) NewChain(table string, chain string) error {
+	// check if the chain exist
+	exists, err := iptutil.ipt.ChainExists(table, chain)
+	if err == nil && !exists {
+		err = iptutil.ipt.NewChain(table, chain)
+		klog.Infof("there is no such chain existv, now start to Newchain")
+	}
+	if err != nil {
+		klog.Errorf("error on iptables.NewChain in table %v for chain %v  err ", table, chain, err)
+		return err
+	}
+	klog.Infof("iptables.NewChain succeeded, in table %v for chain %v ", table, chain)
+	return nil
+}
--- adapter/main.go
+++ adapter/main.go
@@ -8,21 +8,6 @@ import (
 )
 
 func main() {
-	// 读取配置文件
-	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
-	if err != nil {
-		fmt.Println("Error reading config file: ", err)
-		return
-	}
-
-	edgeIP := viper.GetString("edge.ip")
-	edgeMask := viper.GetString("edge.mask")
-	appIP := viper.GetString("app.ip")
-	appPort := viper.GetString("app.port")
-
 	// 初始化iptables
 	ipt, err := iptables.New()
 	if err != nil {
@@ -30,14 +15,14 @@ func main() {
 		return
 	}
 
-	// 创建EdgeMesh链
+	// 在 nat table 创建EdgeMesh链
 	err = ipt.NewChain("nat", "EdgeMesh")
 	if err != nil {
 		fmt.Println("Error creating EdgeMesh chain: ", err)
 		return
 	}
 
-	// 插入规则，将目标地址是edge网段的数据包都拦截转发到应用层的进程
+	// 插入规则，在 PREROUTING 时候将目标地址是edge网段的数据包都拦截转发到应用层的进程
 	ruleSpec := []string{"-s", edgeIP + "/" + edgeMask, "-j", "DNAT", "--to-destination", appIP + ":" + appPort}
 	err = ipt.Append("nat", "PREROUTING", ruleSpec...)
 	if err != nil {
@@ -46,4 +31,52 @@ func main() {
 	}
 
 	fmt.Println("EdgeMesh chain created and rule inserted successfully.")
+}
+
+type Adapter interface {
+	// 配置文件中获取云边的网段地址
+	getCIDR() (cidr, error)
+
+	// 获取 EdgeTunnel 的端口
+	// @TODO 创建独立的 EDGETUNNEL
+	getTunnel() (string, error)
+
+	// 创建 EDGEMESH 链 并依据Tunnel 信息插入转发规则
+	applyRules() (bool, error)
+
+	// 监视表中的规则，如果 Tunnel 或者 Config 文件发生修改，立即修改
+	watchRules() error
+
+	// 修改（增加/删除）表中拦截到 Tunnel 的规则
+	updateRules() error
+}
+
+type cidr struct {
+	cloud []string
+	edge  []string
+}
+
+// 从配置文件中获取不同网段的地址
+func getCIDR() cidr {
+	// 读取配置文件
+	// TODO： 接入到 EdgeMesh 中，需要商量这个配置文件的位置
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Error reading config file: ", err)
+		return nil
+	}
+
+	edgeIP := viper.GetString("edge.ip")
+	cloudIP := viper.GetString("cloud.ip")
+
+	c := &cidr{
+		cloud: cloudIp,
+		edge:  edgeIP,
+	}
+	fmt.Printf("get ip info: edge %s/%s, edgemesh info: %s:%s ", edgeIP, edgeMask, appIP, appPort)
+
+	return c
 }
